71f05e5525c0dc4ebc688eab1101e3422d6724e6
Thanakan <getgorok4@gmail.com>
2018-04-27 01:04:48 +0700

    Test case for test get average

--- src/main/java/camt/se234/lab11/dao/StudentDaoImpl.java
+++ src/main/java/camt/se234/lab11/dao/StudentDaoImpl.java
@@ -10,7 +10,14 @@ public class StudentDaoImpl implements StudentDao {
     public StudentDaoImpl(){
         students = new ArrayList<>();
         students.add(new Student("123","A","temp",2.33));
+        students.add(new Student("456","B","bird",3.00));
+        students.add(new Student("759","","cat",1.0));
+        students.add(new Student("100","D","",4.0));
 
+    }
+    public StudentDaoImpl(String id, String n,String s,double score){
+        students = new ArrayList<>();
+        students.add(new Student(id,n,s,score));
     }
 
     @Override
--- src/test/java/camt/se234/lab11/service/StudentServiceImplTest.java
+++ src/test/java/camt/se234/lab11/service/StudentServiceImplTest.java
@@ -2,9 +2,16 @@ package camt.se234.lab11.service;
 
 import camt.se234.lab11.dao.StudentDaoImpl;
 import camt.se234.lab11.entity.Student;
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
+import junitparams.naming.TestCaseName;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
+
+@RunWith(JUnitParamsRunner.class)
 public class StudentServiceImplTest {
     @Test
     public void testFindById(){
@@ -12,6 +19,37 @@ public class StudentServiceImplTest {
         StudentServiceImpl studentService = new StudentServiceImpl();
         studentService.setStudentDao(studentDao);
         assertThat(studentService.findStudentById("123"),is(new Student("123","A","temp",2.33)));
+        assertThat(studentService.findStudentById("456"),is(new Student("456","B","bird",3.00)));
+        assertThat(studentService.findStudentById("759"),is(new Student("759","","cat",1.0)));
+        assertThat(studentService.findStudentById("100"),is(new Student("100","D","",4.0)));
+
     }
+    @Test
+    public void testGetAverageGpa(){
+        StudentDaoImpl studentDao = new StudentDaoImpl();
+        StudentServiceImpl studentService = new StudentServiceImpl();
+        studentService.setStudentDao(studentDao);
+        assertThat(studentService.getAverageGpa(),is(2.5825));
+    }
+
+    public Object paramsForTestGetAverageGpa(){
+        return new Object[][]{
+                {"111","","",4.0,4.0},
+                {"111","","",3.0,3.0},
+                {"111","","",2.0,2.0}
+
+        };
+    }
+    @Test
+    @Parameters(method = "paramsForTestGetAverageGpa")
+    @TestCaseName("Test getGrade Params [{index}] : input is {0}, expect \"{1}\"")
+    public void testGetAverageGpaparams(String id,String name ,String surname,double score,double expectedGrade){
+        StudentDaoImpl studentDao = new StudentDaoImpl(id,name,surname,score);
+        StudentServiceImpl studentService = new StudentServiceImpl();
+        studentService.setStudentDao(studentDao);
+        assertThat(studentService.getAverageGpa(),is(expectedGrade));
+
+    }
+
 
 }
